d4ed8d8c5d4ceeaf6234eb3b4bcb28847b78b17f
PriyanshuDoshi <doshipriyanshu3@gmail.com>
2025-09-16 09:11:11 +0530

    basic module of medicine

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 //HELLO
+//secure pharmaceutical prescription system
+#include <bits/stdc++.h>
+using namespace std;
+//2 types of the medicine otc and not otc
+class Medicine{};
+
+class otc_med:public Medicine{
+};
+class non_otc_med:public Medicine{
+
+};
+
+int main() {
+
+}
