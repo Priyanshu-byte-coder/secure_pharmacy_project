c1a04e64861595b7898abaf6e12c2418b3c674b2
PriyanshuDoshi <doshipriyanshu3@gmail.com>
2025-10-07 09:00:34 +0530

    userinfo update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
+//for storing the userinfo 
+struct userinfo{
+    string password;
+    string role;
+};
 class User{
     private:
     // int userID;
     string username;
     string password;
-    // char role;
+    string role;
     //file where all the users are stored
     static const string filename;
     protected:
@@ -15,15 +22,15 @@ class User{
     //default constructor
     User(){}
     //parameterized constructor
-    User(string uname,string pass){//,int usid,char r
+    User(string uname,string pass,string r){//,int usid
         username=uname;
         password=pass;
         // userID=usid;
-        // role=r;
+        role=r;
     }
     //if the user is new then register them
     bool registerUser(){
-        unordered_map<string,string>users=loadUsers();
+        unordered_map<string,userinfo>users=loadUsers();
         if(users.find(username)!=users.end()){
             cout<<"User already exists"<<endl;
             return false;
@@ -33,7 +40,7 @@ class User{
             cout<<"ERROR: could not open the file for writing"<<endl;
             return false;
         }
-        fout<<username<<','<<password;
+        fout<<username<<','<<password<<','<<role<<endl;
         fout.close();
         cout<<"Registration successful "<<endl;
         return true;
@@ -41,12 +48,12 @@ class User{
 
     //for loging in the user
     bool loginUser(){
-        unordered_map<string,string>users=loadUsers();
+        unordered_map<string,userinfo>users=loadUsers();
         auto it=users.find(username);
         if(it==users.end()){
             cout<<"No such user found."<<endl;
         }
-        if(password==it->second){
+        if(password==it->second.password){
             cout<<"login successfull !!"<<endl;
             cout<<"Welcome, "<<username<<endl;
         }else{
@@ -57,17 +64,17 @@ class User{
 
 
     //to load the users into the map
-    static unordered_map<string,string>loadUsers(){
-        unordered_map<string,string>mpp;
+    static unordered_map<string,userinfo>loadUsers(){
+        unordered_map<string,userinfo>mpp;
         ifstream fin(filename);
         if(!fin)return mpp;
         string line;
         while(getline(fin,line)){
             if(line.empty())continue;
             stringstream ss(line);
-            string uname,pass;
-            if(getline(ss,uname,',')&&getline(ss,pass)){
-                mpp[uname]=pass;
+            string uname,pass,role;
+            if(getline(ss,uname,',')&&getline(ss,pass,',')&&getline(ss,role)){
+                mpp[uname]={pass,role};
             }
         }
         fin.close();
@@ -91,23 +98,27 @@ int main() {
         getline(cin, choice);
 
         if (choice == "1") {
-            string uname, pass;
+            string uname, pass,role;
             cout << "Enter username: ";
             getline(cin, uname);
             cout << "Enter password: ";
             getline(cin, pass);
+            cout << "Enter role: ";
+            getline(cin, role);
 
-            User newUser(uname, pass);
+            User newUser(uname,pass,role);
             newUser.registerUser();
 
         } else if (choice == "2") {
-            string uname, pass;
+            string uname, pass,role;
             cout << "Enter username: ";
             getline(cin, uname);
             cout << "Enter password: ";
             getline(cin, pass);
+            cout << "Enter role: ";
+            getline(cin, role);
 
-            User loginAttempt(uname, pass);
+            User loginAttempt(uname, pass,role);
             loginAttempt.loginUser();
 
         } else if (choice == "0") {
